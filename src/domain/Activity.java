c3022f02e9ae842cdac71f6de51ad3de2fd9e59a
eduards <edua0193@stud.kea.dk>
2017-09-18 09:51:32 +0200

    activity class getters setters, constructor

--- src/domain/Activity.java
+++ src/domain/Activity.java
@@ -5,5 +5,39 @@ package domain;
  */
 public class Activity {
 
-    String name;
+    private String name;
+    private String description;
+    private String duration;
+
+    public Activity(String name, String description, String duration) {
+        this.name = name;
+        this.description = description;
+        this.duration = duration;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+
 }
